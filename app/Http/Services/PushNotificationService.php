7d95c3c52c01c93154f7a74d8817fa7fe6b63e3b
mohamdfawaz <mohamdfawaz93@gmail.com>
2020-11-04 00:48:21 +0200

    Added new implementation for push notification

--- app/Http/Services/PushNotificationService.php
+++ app/Http/Services/PushNotificationService.php
@@ -13,22 +13,25 @@ class PushNotificationService
 
         $url = 'https://fcm.googleapis.com/fcm/send';
 
+        $msg = array
+        (
+            'title'		=> 'This is a title. title',
+            'subtitle'	=> 'This is a subtitle. subtitle',
+            'tickerText'	=> 'Ticker text here...Ticker text here...Ticker text here',
+            'vibrate'	=> 1,
+            'sound'		=> 1,
+            'largeIcon'	=> 'large_icon',
+            'smallIcon'	=> 'small_icon',
+            "message" => $message,
+            "amount" => $amount,
+            "action" => $operation,
+            "currency" => $currency
+        );
         $fields = array(
             'registration_ids' => array(
                 $id
             ),
-            'data' => array(
-                "message" => $message,
-                "amount" => $amount,
-                "action" => $operation,
-                "currency" => $currency
-            ),
-            'notification' => array(
-                "message" => $message,
-                "amount" => $amount,
-                "action" => $operation,
-                "currency" => $currency
-            )
+            'data' => $msg
         );
         $fields = json_encode($fields);
 
